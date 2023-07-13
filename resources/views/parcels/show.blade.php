c735dec7d9fde231a8372d54799e96c04ce26927
mirzabadri <19mmb99.gamerz@gmail.com>
2023-07-13 16:11:03 +0800

    redesign tracking history on parcel detail (user)

--- resources/views/parcels/show.blade.php
+++ resources/views/parcels/show.blade.php
@@ -30,6 +30,93 @@
 
     <!-- Template Main CSS File -->
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+
+    <!-- Custom CSS for timeline -->
+    <style>
+        .timeline {
+            position: relative;
+            padding: 20px 0;
+        }
+
+        .timeline::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            /* Changed from left: 50% */
+            width: 2px;
+            background-color: #ccc;
+        }
+
+        .timeline-event {
+            position: relative;
+            margin-bottom: 30px;
+        }
+
+        .timeline-event::before {
+            content: "";
+            position: absolute;
+            top: 5px;
+            left: -10px;
+            width: 20px;
+            height: 20px;
+            border-radius: 50%;
+            border: 2px solid #ccc;
+        }
+
+        .timeline-event:nth-child(1)::before {
+            background-color: #ff0000; /* Red */
+        }
+
+        .timeline-event:nth-child(2)::before {
+            background-color: #00ff00; /* Green */
+        }
+
+        .timeline-event:nth-child(3)::before {
+            background-color: #0000ff; /* Blue */
+        }
+
+        .timeline-event:nth-child(4)::before {
+            background-color: #ff00ff; /* Magenta */
+        }
+
+        .timeline-event:nth-child(5)::before {
+            background-color: #ffa500; /* Orange */
+        }
+
+        .timeline-event:nth-child(n+2)::before,
+        .timeline-event:nth-child(n+2)::after {
+            /* Line color from previous point */
+            border-color: var(--prev-color);
+        }
+
+        .timeline-event:nth-child(n+2)::after {
+            content: "";
+            position: absolute;
+            left: -5px;
+            top: 13px;
+            height: calc(100% - 10px);
+            width: 2px;
+            background-color: var(--prev-color);
+        }
+
+        /* Add more color styles for additional timeline events */
+
+        .timeline-event .event-date {
+            margin-bottom: 8px;
+            font-weight: 600;
+            position: relative;
+            /* Added */
+            left: 30px;
+            /* Added */
+        }
+
+        .timeline-event .event-description {
+            padding-left: 34px;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -49,91 +136,77 @@
                 <p><strong>Weight:</strong> {{ $parcel->weight }}</p>
                 <p><strong>Description:</strong> {{ $parcel->description }}</p>
                 <p><strong>Tracking Number:</strong> {{ $parcel->tracking_number }}</p>
-                <p><strong>Status:</strong>
-                    @if ($parcel->status === 'created')
-                        Order Created
-                    @elseif ($parcel->status === 'picked_up')
-                        Parcel has been picked up
-                    @elseif ($parcel->status === 'departed')
-                        Parcel has departed from the sorting facility
-                    @elseif ($parcel->status === 'out_for_delivery')
-                        Parcel is out for delivery
-                    @elseif ($parcel->status === 'delivered')
-                        Parcel has been delivered
-                    @else
-                        Unknown
-                    @endif
-                </p>
-                <button id="show-qr-code" class="btn btn-primary mb-2">Show QR Code</button>
+                <button id="show-qr-code" class="btn btn-primary mb-2" data-bs-toggle="modal"
+                    data-bs-target="#qrModal">Show QR Code</button>
                 <!-- Button to show the QR code -->
-
                 <button id="show-tracking-history" class="btn btn-primary mb-2">Show Tracking History</button>
                 <!-- Button to show the tracking history -->
-
             </div>
             <div class="card-footer">
                 <a href="{{ route('parcels.index') }}" class="btn btn-secondary">Back</a>
                 <a href="{{ route('parcels.edit', $parcel) }}" class="btn btn-primary">Edit</a>
             </div>
         </div>
-
     </main>
 
     @include('layouts.footer')
 
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
     <script>
         document.getElementById('show-qr-code').addEventListener('click', function() {
-            const qrCodeSvg = `{!! QrCode::size(300)->format('svg')->generate($parcel->tracking_number) !!}`;
-            const qrModalBody = document.createElement('div');
-            qrModalBody.innerHTML = qrCodeSvg;
+            const qrCodeImage = document.getElementById('qr-code-image');
 
-            const qrModal = new bootstrap.Modal(document.getElementById('qrModal'));
-            qrModalBody.classList.add('text-center', 'mx-auto');
-            qrModalBody.style.maxWidth = '90vw';
-            qrModalBody.style.width = '90%';
-
-            const modalContent = document.querySelector('#qrModal .modal-content');
-            modalContent.innerHTML = '';
-            modalContent.appendChild(qrModalBody);
-
-            qrModal.show();
+            qrCodeImage.src =
+                `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={{ $parcel->tracking_number }}`;
         });
 
         document.getElementById('show-tracking-history').addEventListener('click', function() {
-            // Fetch tracking history data
-            console.log('Fetching tracking history...');
             fetch(`/parcels/{{ $parcel->id }}/tracking-history`)
                 .then(response => response.json())
                 .then(data => {
-                    console.log('Received tracking history data:', data);
-
                     const trackingList = document.getElementById('tracking-list');
                     trackingList.innerHTML = '';
 
-                    // Display tracking history
-                    data.forEach(tracking => {
-                        const row = document.createElement('tr');
-                        const timestampCell = document.createElement('td');
-                        const statusCell = document.createElement('td');
+                    let prevColor = null;
 
-                        let formattedDate = 'N/A'; // Default value if updated_at is undefined
+                    data.forEach((tracking, index) => {
+                        const event = document.createElement('div');
+                        event.classList.add('timeline-event');
+                        event.style.setProperty('--prev-color', prevColor);
 
-                        if (tracking.updated_at) {
-                            const updatedDate = new Date(tracking.updated_at.replace(' ',
-                                'T')); // Convert to ISO 8601 format
-                            formattedDate = updatedDate
-                                .toLocaleString(); // Change the date format if needed
+                        const date = document.createElement('div');
+                        date.classList.add('event-date');
+                        date.textContent = tracking.updated_at || 'N/A';
+                        event.appendChild(date);
+
+                        const description = document.createElement('div');
+                        description.classList.add('event-description');
+                        description.textContent = tracking.status;
+                        event.appendChild(description);
+
+                        trackingList.appendChild(event);
+
+                        if (index === 0) {
+                            prevColor = getComputedStyle(event).getPropertyValue('background-color');
+                        } else {
+                            prevColor = getComputedStyle(event.previousSibling).getPropertyValue('background-color');
                         }
-
-                        timestampCell.textContent = formattedDate;
-                        statusCell.textContent = tracking.status;
-
-                        row.appendChild(timestampCell);
-                        row.appendChild(statusCell);
-                        trackingList.appendChild(row);
                     });
 
-                    // Show the tracking history modal
                     const trackingModal = new bootstrap.Modal(document.getElementById('trackingModal'));
                     trackingModal.show();
                 })
@@ -143,7 +216,8 @@
         });
     </script>
 
-    <!-- Modal -->
+    <!-- Modals... -->
+    <!-- QR Code Modal -->
     <div class="modal fade" id="qrModal" tabindex="-1" aria-labelledby="qrModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
             <div class="modal-content">
@@ -151,7 +225,9 @@
                     <h5 class="modal-title" id="qrModalLabel">QR Code</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body"></div>
+                <div class="modal-body text-center">
+                    <img id="qr-code-image" src="" alt="QR Code" class="img-fluid">
+                </div>
             </div>
         </div>
     </div>
@@ -161,19 +237,13 @@
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="trackingModalLabel">Tracking History</h5>
+                    <h5 class="modal-title" id="trackingModalLabel">Parcel Movement</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Timestamp</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody id="tracking-list"></tbody>
-                    </table>
+                    <div class="timeline">
+                        <div id="tracking-list"></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -181,20 +251,3 @@
 </body>
 
 </html>
-
-<!-- Vendor JS Files -->
-<script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
-<script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-<script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
-
-<!-- Template Main JS File -->
-<script src="{{ asset('assets/js/main.js') }}"></script>
-
-<!-- Vendor JS Files -->
-<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
